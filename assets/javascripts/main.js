c774933f1c9aa5caeae837f3c6cb251c1abc4f7a
Marcus Lilja <marcus.lilja@gmail.com>
2014-02-18 21:55:20 +0100

    Add console-browserify to fix issues with IE and console.log

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,16 +2,19 @@
   'use strict';
 
   var $ = require('../../bower_components/jquery/dist/jquery.js'),
-    Test = require('./modules/test.js'),
-    test = new Test();
-
-  test.init();
+    console = require('console-browserify'),
+    Test = require('./modules/test.js');
 
   // Define jQuery globals for plugin handling
+  // Define console to work with more browsers
   window.$ = $;
   window.jQuery = $;
+  window.console = console;
 
   $(function () {
+    var test = new Test();
+    test.init();
+
     $('.container').append($('<p>jQuery loaded!</p>'));
   });
 }());
