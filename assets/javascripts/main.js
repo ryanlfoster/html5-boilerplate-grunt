75063853a2e271cda0c07b4330817819bbfa9b48
Marcus Lilja <marcus.lilja@gmail.com>
2014-03-08 16:24:13 +0100

    Minor change to default script

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,14 +2,14 @@
   'use strict';
 
   var $ = require('jquery'),
-    Test = require('modules/test');
+    Test = require('modules/test'),
+    test = new Test();
 
   // Define jQuery globals for plugin handling
   window.$ = $;
   window.jQuery = $;
 
   $(function () {
-    var test = new Test();
     test.init();
 
     $('.container').append($('<p>jQuery loaded!</p>'));
