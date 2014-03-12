b03a1a7e92f9f9d8d1c42653c94c0df68ddd9601
Marcus Lilja <marcus.lilja@gmail.com>
2014-03-12 01:51:41 +0100

    Add grunt-contrib-clean

---
+++ grunt/clean.js
@@ -0,0 +1,13 @@
+
+module.exports = function () {
+  'use strict';
+
+  return {
+    clean: [
+      'public/stylesheets',
+      'public/javascripts',
+      'public/images',
+      'public/fonts'
+    ]
+  };
+};
