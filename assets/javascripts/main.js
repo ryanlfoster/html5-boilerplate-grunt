70216e7640d7051390f18688b9b59962c951572f
Marcus Lilja <marcus.lilja@gmail.com>
2014-05-23 01:01:50 +0200

    Remove wrapping functions, not needed

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,18 +1,16 @@
 
-(function () {
-  'use strict';
+'use strict';
 
-  var $ = require('jquery'),
-    Test = require('./modules/test.js'),
-    test = new Test();
+var $ = require('jquery'),
+  Test = require('./modules/test.js'),
+  test = new Test();
 
-  // Define jQuery globals for plugin handling
-  window.$ = $;
-  window.jQuery = $;
+// Define jQuery globals for plugin handling
+window.$ = $;
+window.jQuery = $;
 
-  $(function () {
-    test.init();
+$(function () {
+  test.init();
 
-    $('.container').append($('<p>jQuery loaded!</p>'));
-  });
-}());
+  $('.container').append($('<p>jQuery loaded!</p>'));
+});
--- assets/javascripts/modules/test.js
+++ assets/javascripts/modules/test.js
@@ -1,16 +1,14 @@
 
-(function () {
-  'use strict';
+'use strict';
 
-  function Test() {
-    this.message = 'Some message.';
+function Test() {
+  this.message = 'Some message.';
+}
+
+Test.prototype = {
+  init: function () {
+    console.log(this.message);
   }
+};
 
-  Test.prototype = {
-    init: function () {
-      console.log(this.message);
-    }
-  };
-
-  module.exports = Test;
-}());
+module.exports = Test;
