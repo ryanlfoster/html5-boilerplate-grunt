4d1a3065710c9285e26683b1d9fd9395c3ced885
Marcus Lilja <marcus.lilja@gmail.com>
2014-06-11 17:04:36 +0200

    Update default autoprefixer settings

--- grunt/autoprefixer.js
+++ grunt/autoprefixer.js
@@ -4,7 +4,7 @@
 module.exports = function () {
   return {
     options: {
-      browsers: ['last 2 version', 'ie 8', 'ie 9'],
+      browsers: ['last 3 version', 'ie 9'],
       map: false
     },
 
