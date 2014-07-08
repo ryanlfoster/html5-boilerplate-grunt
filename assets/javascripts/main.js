6a79006d02c40739efa5bbae9630a9e3b0e9586c
Marcus Lilja <marcus.lilja@gmail.com>
2014-07-08 15:34:54 +0200

    Remove template module and add .gitkeeps

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,15 +1,12 @@
 
 'use strict';
 
-var $ = require('jquery'),
-  message = require('./modules/message.js');
+var $ = require('jquery');
 
 // Define jQuery globals for plugin handling
 window.$ = $;
 window.jQuery = $;
 
 $(function () {
-  message.print('Some message');
-
   $('.container').append($('<p>jQuery loaded!</p>'));
 });
--- assets/javascripts/modules/message.js
+++
@@ -1,8 +0,0 @@
-
-'use strict';
-
-module.exports = {
-  print: function (message) {
-    console.log(message);
-  }
-};
